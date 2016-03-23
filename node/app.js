8c18ef2440c753c08ff780f8cb3a1735b102c2c7
Jason Katz <jkatz94@gmail.com>
2016-03-23 12:27:29 -0400

    Using regular sockets for java-node connection

--- node/app.js
+++ node/app.js
@@ -2,10 +2,24 @@ var app = require( 'express' )();
 var http = require( 'http' ).Server( app );
 var io = require( 'socket.io' )( http );
 var unixSocket = require( 'unix-socket' );
+var net = require('net');
 var ROOT = '/home/jason/school/ece361/set';
 
 app.get( '/', function( req, res ){
     res.sendFile( ROOT + '/public_html/index.html' );
+});
+
+const client = net.connect( 1010, 'localhost' );
+client.on( 'connect', function() {
+    // 'connect' listener
+    console.log( 'connected to server!' );
+    client.write( 'connected to server\n' );
+});
+client.on( 'data', function( data ) {
+    console.log( 'message received: ' + data.toString() );
+});
+client.on( 'end', function() {
+    console.log( 'disconnected from server' );
 });
 
 http.listen( 3000, function(){
@@ -13,24 +27,19 @@ http.listen( 3000, function(){
 });
 
 io.on( 'connection', function( socket ) {
+    client.write( 'a user connected\n' );
     console.log( 'a user connected' );
     socket.on( 'SET', function( msg ) {
         io.emit( 'SET', msg );
         var dt = msg - new Date().getTime();
         console.log( 'SET:' +  + ' ' + dt.toString() );
     });
-});
-
-var option = { 
-    path: ROOT + '/server.sock',
-    mode: 0666 
-};
-
-unixSocket.listen( app, option, function( result ) {
-    if( result ) {
-        console.log( 'Server started on ' + result );
-    } else {
-        console.error( 'Error' );
-        process.exit( 0 );
-    }
+    socket.on( 'PING_ALL', function( msg ) {
+        io.emit( 'PING_ALL', msg );
+        console.log( 'pinged' );
+    });
+    socket.on( 'disconnect', function() {
+        client.write( 'a user disconnected\n' );
+        console.log( 'a user disconnected' );
+    });
 });

44fb4f9c7796f4a2e2907388c79621f0b892bbdf
Jason Katz <jkatz94@gmail.com>
2016-03-29 10:54:41 -0400

    Some java stuff for routing

--- java/SetServer.java
+++ java/SetServer.java
@@ -5,13 +5,19 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.json.*;
 
-public class Test {
+public class SetServer {
 
     private static int msgCounter = 0;
 
     public static void main(String[] args) {
-        try (
+        String json = "{\"method\": \"methodTest\", \"data\": \"dataTest\"}";
+        RequestObject obj = RequestObject.fromJSON(json);
+        System.out.println("METHOD: " + obj.method);
+        System.out.println("DATA: " + obj.data);
+        return;
+        /*try (
             ServerSocket server = new ServerSocket(1010);
             Socket client = server.accept();
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
@@ -20,29 +26,46 @@ public class Test {
             String inputLine, outputLine;
 
             // Initiate conversation with client
-            outputLine = Test.processInput(null);
+            outputLine = SetServer.processInput(null);
             System.out.println(outputLine);
             out.println(outputLine);
 
             while ((inputLine = in.readLine()) != null) {
-                outputLine = Test.processInput(inputLine);
+                outputLine = SetServer.processInput(inputLine);
                 System.out.println(outputLine);
                 out.println(outputLine);
                 if (outputLine.equals("STOP")) {
                     break;
-                } else if (outputLine.equals("PING_ALL")) {
-                    // I DON'T THINK THIS IS ACTUALLY RUNNING
-                    System.out.println("Pinging all clients");
-                    out.println("PING_ALL");
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public static String processInput(String input) {
+        if (input.equals("PING_ALL")) {
+            // This isn't running...
+            System.out.println("Pinging all clients");
+        }
         return "Processed message " + msgCounter++ + ": " + input;
+    }
+
+    protected class RequestObject {
+        public string method;
+        public string data;
+
+        public RequestObject(String method, String data) {
+            this.method = method;
+            this.data = data;
+        }
+
+        public static RequestObject fromJSON(string json) {
+            JSONObject obj = new JSONObject(json);
+            String m = obj.getString("method");
+            String d = obj.getString("data");
+            return RequestObject(m, d);
+        }
     }
 
 }

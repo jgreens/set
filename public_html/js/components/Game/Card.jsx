ed579ccb86888056f9538c08fe66f05eca702040
Jonny Greenspan <greens@cooper.edu>
2016-05-01 13:30:09 -0400

    Selecting a set works, styled

--- public_html/js/components/Game/Card.jsx
+++ public_html/js/components/Game/Card.jsx
@@ -6,10 +6,26 @@ define(
     React
 ) {
     var Card = React.createClass({
+        getInitialState: function() {
+            return {
+                selected: false,
+                className: 'ui fluid card'
+            };
+        },
+        _toggleSelected: function() {
+            var selectCards = this.props.selectCards();
+            if( this.state.selected ) {
+                if( selectCards.remove( this.props.card ) )
+                    this.setState({ selected: false, className: 'ui fluid card' });
+            } else {
+                if( selectCards.add( this.props.card ) )
+                    this.setState({ selected: true, className: 'ui fluid teal card' });
+            } 
+        },
         render: function() {
             return(
-                <div className="Card column">
-                    <div className="ui fluid card">
+                <div className="Card column" onClick={this._toggleSelected}>
+                    <div className={this.state.className}>
                         <div className="content">
                             <img src={'img/cards/' + this.props.card + '.png'} style={{width: '100%'}} />
                         </div>
--- public_html/js/components/Game/Game.jsx
+++ public_html/js/components/Game/Game.jsx
@@ -14,7 +14,8 @@ define(
             return {
                 cards: [],
                 feed: [],
-                scores: {}
+                scores: {},
+                selected: {}
             };
         },
         componentWillMount: function() {
@@ -28,7 +29,6 @@ define(
             var self = this;
             Socket.startGame( function( data ) {
                 console.log( 'GAME UPDATE' );
-console.log( data );
                 self.setState( data );
             });
         },
@@ -38,15 +38,51 @@ console.log( data );
                 console.log( 'RETURN TO LOBBY' );
             });
         },
+        _selectCards: function() {
+            var cardManip = {};
+            var self = this;
+            cardManip.count = function() {
+                var count = 0;
+
+                for( var key in self.state.selected )
+                    if( self.state.selected.hasOwnProperty( key ) )
+                        count++;
+
+                return count;
+            };
+            cardManip.add = function( card ) {
+                // Only select if less than three cards currently selected
+                if( cardManip.count() < 3 ) {
+                    var selected = self.state.selected;
+                    selected[card] = true;
+                    self.setState({ selected: selected });
+
+                    return true;
+                }
+
+                return false;
+            };
+            cardManip.remove = function( card ) {
+                var selected = self.state.selected;
+                delete selected[card];
+                self.setState({ selected: selected });
+
+                return true;
+            };
+
+            return cardManip;
+        },
         render: function() {
             return(
                 <div className="Game ui grid container">
                     <div className="row">
-                        <div className="ten wide column">
-                            <GameBoard cards={this.state.cards} />
+                        <div className="eight wide column">
+                            <GameBoard cards={this.state.cards} selectCards={this._selectCards} />
+                            <button className="huge fluid ui teal button" style={{marginTop: '25px'}}>SET</button>
+                        </div>
+                        <div className="two wide column">
                         </div>
                         <div className="six wide column">
-                            <GameBoard cards={this.state.cards} />
                         </div>
                     </div>
                 </div>

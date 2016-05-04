60436e10d4aef55a5e21f1054614d5abb6403813
Jonny Greenspan <greens@cooper.edu>
2016-05-03 22:21:36 -0400

    Adding user icons to feed

--- public_html/js/components/Game/Event.jsx
+++ public_html/js/components/Game/Event.jsx
@@ -41,14 +41,40 @@ define(
             }
             return {
                 content: content,
-                className: className
+                className: className,
+                obj: obj
             };
+        },
+        _additionalHTML: function() {
+            var html = [];
+
+            switch( this.state.obj.msgType ) {
+                case 'chat':
+                    // Generating icon
+                    var seed = 0;
+                    var name = this.state.obj.username;
+
+                    for( var j = 0; j < name.length; j++ )
+                        seed += ( name.charCodeAt(j) * j * 37 );
+                    seed = seed.toString( 16 );
+                    var hash = '327b8763d4f0039dab25572ee873caaa';
+                    hash = hash.substring( 0, hash.length - seed.length ) + seed;
+                    var url = 'http://www.gravatar.com/avatar/' + hash + '?s=30&d=identicon&r=PG';
+
+                    html.push( <img src={url} className="ui avatar image" /> );
+                    break;  
+                default:
+                    break;
+            }
+
+            return html;
         },
         render: function() {
             return(
                 <div className="Event event">
                     <div className="content">
                         <div className={this.state.className}>
+                            {this._additionalHTML()}
                             {this.state.content}
                         </div>
                     </div>

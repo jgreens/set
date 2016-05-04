fd2cdf2c107d67f952d6d3cf81c4eae93a6ff84c
Jonny Greenspan <greens@cooper.edu>
2016-05-04 00:15:14 -0400

    #34 scoreboard works

--- public_html/js/components/Game/Scoreboard.jsx
+++ public_html/js/components/Game/Scoreboard.jsx
@@ -8,17 +8,33 @@ define(
     var Scoreboard = React.createClass({
         _generateScoreboard: function() {
             var html = [];
+
             var scores = this.props.scores;
             var user = this.props.user;
 
+            var reversed = {};
+            var list = [];
             for( var key in scores ) {
-                var className = ( key == user.name ) ? 'ui small teal label' : 'ui small label';
-                html.push( 
-                    <div key={key} className="item">
-                        <div className={className}>{scores[key]}</div>
-                        {key}
-                    </div>
-                );
+                if( typeof reversed[scores[key]] == 'undefined' ) {
+                    reversed[scores[key]] = [];
+                    list.push(scores[key]);
+                }
+                reversed[scores[key]].push({ name: key, score: scores[key] });
+            }
+            list.sort(function(a,b){return b-a});
+
+            for( var i = 0; i < list.length; i++ ) {
+                var sameScore = reversed[list[i]];
+                for( var j = 0; j < sameScore.length; j++ ) {
+                    var score = reversed[list[i]][j];
+                    var className = ( score.name == user.name ) ? 'ui small teal label' : 'ui small label';
+                    html.push( 
+                        <div key={score.name} className="item">
+                            <div className={className}>{score.score}</div>
+                            {score.name}
+                        </div>
+                    );
+                }
             }
 
             return html;

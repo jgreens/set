1cb6e99ffd334b800957039b2b903fc8c89a0d36
Jason Katz <jkatz94@gmail.com>
2020-04-05 13:55:49 -0400

    Use member nomenclature for game participants

--- node/games.js
+++ node/games.js
@@ -1,31 +1,31 @@
 let games = {};
 
-const addUserToGame = (id, userId) => {
+const addMemberToGame = (id, clientId) => {
     if (!games[id]) {
-        console.error(`Cannot add user to game with id ${id} - game does not exist`);
+        console.error(`Cannot add client to game with id ${id} - game does not exist`);
         return;
     }
 
-    games[id].users.push(userId);
+    games[id].members.push(clientId);
 };
 
-const removeUserFromGame = (id, userId) => {
+const removeMemberFromGame = (id, clientId) => {
     if (!games[id]) {
-        console.error(`Cannot remove user from game with id ${id} - game does not exist`);
+        console.error(`Cannot remove client from game with id ${id} - game does not exist`);
         return;
     }
 
-    const userIndex = games[id].users.indexOf(userId);
+    const memberIndex = games[id].members.indexOf(clientId);
 
-    if (userIndex < 0) {
-        console.error(`Cannot remove user with id ${userId} from game with id ${id} - user not in game`);
+    if (memberIndex < 0) {
+        console.error(`Cannot remove client with id ${clientId} from game with id ${id} - client not in game`);
         return;
     }
 
-    games[id].users.splice(userIndex, 1);
+    games[id].members.splice(memberIndex, 1);
 };
 
 module.exports = {
-    addUserToGame,
-    removeUserFromGame,
+    addMemberToGame,
+    removeMemberFromGame,
 };
